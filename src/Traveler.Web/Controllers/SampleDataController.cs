4b6a239b3257c41f3f81b0c4e4225b00b6250d85
Marcin Bigos <marcin.bigos99@gmail.com>
2019-06-07 16:14:59 +0200

    rename to traveler

--- src/Traveler.Web/Controllers/SampleDataController.cs
+++ src/Traveler.Web/Controllers/SampleDataController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Traveller.Web.Controllers
+namespace Traveler.Web.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
--- src/Traveler.Web/Program.cs
+++ src/Traveler.Web/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Traveller.Web
+namespace Traveler.Web
 {
     public class Program
     {
