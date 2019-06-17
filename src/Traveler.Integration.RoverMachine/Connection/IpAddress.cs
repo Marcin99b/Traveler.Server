bcaae210d65cbe165d59e75a5c661b8cc847267e
Marcin Bigos <marcin.bigos99@gmail.com>
2019-06-17 20:44:02 +0200

    add tcp raw client

---
+++ src/Traveler.Integration.RoverMachine/Connection/IpAddress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Traveler.Integration.RoverMachine.Connection
+{
+    public class IpAddress
+    {
+        public string Ip { get; private set; }
+        public int Port { get; private set; }
+
+        public IpAddress(string ip, int port)
+        {
+            Ip = ip;
+            Port = port;
+        }
+    }
+}
--- src/Traveler.Integration.RoverMachine/Connection/TcpRawClient.cs
+++ src/Traveler.Integration.RoverMachine/Connection/TcpRawClient.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Text;
 
 namespace Traveler.Integration.RoverMachine.Connection
 {
-    public class TcpRawClient
+    public class TcpRawClient : IDisposable
     {
+        private readonly TcpClient tcpClient;
+        public bool IsConnected => tcpClient.Connected;
+
+        public TcpRawClient(IpAddress ipAddress)
+        {
+            this.tcpClient = new TcpClient(ipAddress.Ip, ipAddress.Port);
+        }
+
+        public void Send(byte[] message)
+        {
+            using (var stream = tcpClient.GetStream())
+            {
+                stream.Write(message, 0, message.Length);
+            }
+        }
+
+        public void Dispose()
+        {
+            this.tcpClient.Dispose();
+        }
     }
 }
