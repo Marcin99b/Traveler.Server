6fec9b398aaa78ec587f88bb279c004e56b9125d
Marcin Bigos <marcin.bigos99@gmail.com>
2019-06-17 19:21:06 +0200

    add not finished frontend implementation of steering

--- src/Traveler.Web/ClientApp/src/app/home/home.component.ts
+++ src/Traveler.Web/ClientApp/src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@aspnet/signalr';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -10,47 +9,17 @@ import { Observable } from "rxjs";
 
 export class HomeComponent implements OnInit {
 
-  private hubConnection: HubConnection;
-  nick = '';
-  message = '';
-  messages: any[] = [];
-  messages$: Observable<any[]>;
-
   constructor(private httpClient: HttpClient) {
-    this.messages$.subscribe(x => this.messages);
   }
 
-
-
-  ngOnInit() {
-    this.nick = window.prompt('Your name:', 'John');
-    
-    this.hubConnection = new HubConnectionBuilder().withUrl("/signalr").build();
-
-    this.hubConnection
-      .start()
-      .then(() => console.log('Connection started!'))
-      .catch(err => console.log('Error while establishing connection :('));
-
-    this.hubConnection.on('sendToAll', (nick, text) => {
-      this.messages.push({ nick: nick, text: text });
-    });
-
+  ngOnInit(): void {
   }
 
-  public sendMessage(): void {
-
-    console.log(this.hubConnection.state);
-    this.hubConnection.invoke('sendToAll', this.nick, this.message)
-      .then(() => {
-        this.message = "";
-      })
-      .catch(err => console.log(err));
+  powerChange(event) {
+    console.log("power: " + event.value + "%");
   }
 
-}
-
-interface MessageInfo {
-  name: string;
-  message: string;
+  steeringChange(event) {
+    console.log("steering: " + event.value + "%");
+  }
 }
