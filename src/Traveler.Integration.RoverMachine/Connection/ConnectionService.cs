ca8bdf36219143626498bc4362c33fa13b2f797a
Marcin Bigos <marcin.bigos99@gmail.com>
2019-06-18 02:06:32 +0200

    add tcp facade to connection service

--- src/Traveler.Integration.RoverMachine/Connection/ConnectionService.cs
+++ src/Traveler.Integration.RoverMachine/Connection/ConnectionService.cs
@@ -6,6 +6,13 @@ namespace Traveler.Integration.RoverMachine.Connection
 {
     public class ConnectionService : IConnectionService
     {
+        private readonly ITcpFacade _tcpFacade;
+
+        public ConnectionService(ITcpFacade tcpFacade)
+        {
+            _tcpFacade = tcpFacade;
+        }
+
         public void SendCommand<T>(T command) where T : ICommand
         {
             throw new NotImplementedException();
