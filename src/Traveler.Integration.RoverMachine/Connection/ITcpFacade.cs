130bd35e4a1484f49cc1a1d969f83f560231a1b7
Marcin Bigos <marcin.bigos99@gmail.com>
2019-06-18 02:05:24 +0200

    add code to tcp facade

---
+++ src/Traveler.Integration.RoverMachine/Connection/ITcpFacade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Traveler.Integration.RoverMachine.Connection
+{
+    public interface ITcpFacade
+    {
+        void Send(string message);
+        void Send(byte[] bytes);
+        void Send(ICommand command);
+    }
+}
--- src/Traveler.Integration.RoverMachine/Connection/TcpFacade.cs
+++ src/Traveler.Integration.RoverMachine/Connection/TcpFacade.cs
@@ -4,7 +4,35 @@ using System.Text;
 
 namespace Traveler.Integration.RoverMachine.Connection
 {
-    public class TcpFacade
+    public class TcpFacade : ITcpFacade
     {
+        private readonly IpAddress _ipAddress;
+
+        public TcpFacade(IpAddress ipAddress)
+        {
+            _ipAddress = ipAddress;
+        }
+
+        public void Send(string message)
+        {
+            var bytes = Encoding.UTF8.GetBytes(message);
+            using (var client = new TcpRawClient(this._ipAddress))
+            {
+                client.Send(bytes);
+            }
+        }
+
+        public void Send(byte[] bytes)
+        {
+            using (var client = new TcpRawClient(this._ipAddress))
+            {
+                client.Send(bytes);
+            }
+        }
+
+        public void Send(ICommand command)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
